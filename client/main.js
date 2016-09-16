a8ac793afdde86187f2c33e3cc4441fd0dae0dde
Jordi Campeny <jcampeny@elkanodata.com>
2016-09-16 14:22:15 +0200

    Last Tutorial commit

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-import {name as Enantio} from '../imports/ui/components/core/core';
+import {name as App} from '../imports/ui/components/core/core';
