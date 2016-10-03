f9c4aa77fc784eb446fcc7b9143900e9119f6696
Jordi Campeny <jcampeny@elkanodata.com>
2016-10-03 19:00:29 +0200

    Copia seguridad 3/10

--- imports/ui/components/navigationInputs/inputTag/inputTag.js
+++ imports/ui/components/navigationInputs/inputTag/inputTag.js
@@ -6,7 +6,7 @@ import template from './inputTag.html';
 
 
 class InputTag {
-	constructor ($scope, $reactive) {
+	constructor ($scope, $reactive, $rootScope) {
 		'ngInject';
 
 		$reactive(this).attach($scope);
@@ -15,6 +15,11 @@ class InputTag {
 
 		this.subscribe('countries');
 
+		this.limit = 5;
+		this.expanded = false;
+
+		this.root = $rootScope;
+
 		this.helpers({
 			countries(){
 				return Countries.find({
@@ -22,16 +27,74 @@ class InputTag {
 				    	$regex: new RegExp('^' + this.getReactively('searchText')),
 				    	$options : 'i'
 				  	}
-				}, {limit : 5});
+				}, {limit : this.limit});
 			}
+		});
+
+		$('html').click((event) =>{
+			this.searchText = '';
+			this.contractHeader(event);
+			$scope.$apply();
+		});
+
+		this.root.$on('expandedHeader', (event, data) => {
+			if(data.name == this.name)
+				this.expanded = true;
+		});
+
+		this.root.$on('contractedHeader', (event, data) => {console.log(data);
+			this.expanded = false;
+		});
+
+	}
+
+	selectCountry (country, event) {
+		if(!this.countryAreSelected(country).found){
+			this.selected.push(country);
+			this.searchText = '';
+			this.contractHeader(event);
+			this.root.$broadcast('closeCountries', {
+				event : event
+			});
+		}
+	}
+
+	removeCountry (country) {
+		var index = this.countryAreSelected(country).item;
+
+		this.selected.splice(index,1);
+	}
+
+	countryAreSelected (country) {
+		var found = false;
+		var index = null;
+
+		angular.forEach(this.selected, (countryItem, i) => {
+			if (country._id == countryItem._id){
+				found = true;
+				index = i;
+			}
+		});
+
+		return {
+			found : found,
+			index : index
+		};
+	}
+
+	expandHeader (event) {
+		this.root.$broadcast('expandHeader', {
+			name : this.name,
+			event : event
 		});
 	}
 
-	selectCountry (country) {
-		this.selected = country;
-		console.log(country);
+	contractHeader (event) {
+		this.root.$broadcast('contractHeader', {
+			name : this.name,
+			event : event
+		});
 	}
-
 
 };
 
--- imports/ui/components/navigationInputs/navigationInputs.js
+++ imports/ui/components/navigationInputs/navigationInputs.js
@@ -8,12 +8,15 @@ import { name as InputTag } from './inputTag/inputTag';
 import template from './navigationInputs.html';
 
 class NavigationInputs {
-	constructor ($scope, $reactive, screenSize) {
+	constructor ($scope, $reactive, $rootScope, screenSize, $timeout) {
 		'ngInject';
 
 		$reactive(this).attach($scope);
 
+		this.timeout = $timeout;
+
 		this.sectionSelected = '';
+		this.root = $rootScope;
 
 		this.isMobile = screenSize.is('xs, sm');
 		this.isDesktop = !screenSize.is('xs, sm');
@@ -21,14 +24,110 @@ class NavigationInputs {
 		screenSize.on('xs, sm',(match) =>{
 		    this.isDesktop = !match;
 		    this.isMobile = match;
+		    this.resizeInputs();
 		});	
+
+		$('html').click((event) =>{
+			this.onSectionSelected('', event);
+			$scope.$apply();
+		});
+
+		$(document).ready(() => { this.resizeInputs();});
+
+		this.producto   = [];
+		this.importador = [];
+		this.exportador = [];
+
+		this.root.$on('expandHeader', (event, data) => {
+			this.expandHeader(data.name, data.event);
+		});
+
+		this.root.$on('contractHeader', (event, data) => {
+			this.contractHeader(data.name, data.event);
+		});
+
+		this.root.$on('closeCountries', (event, data) => {
+			this.onSectionSelected('', data.event);
+		});
+		
 	}
 
-	onSectionSelected (section) {
+	onSectionSelected (section = '', event) {
+		if(event)
+			event.stopPropagation();
+
+		if(section != this.sectionSelected && section != '')
+			this.contractHeader(this.sectionSelected, event);
+
 		this.sectionSelected = section;
+
 		$('#input-' + section).focus();
+
+		if(this.isMobile)
+			this.timeout(() => {this.resizeInputs();}, 400);
 	}
 
+	expandHeader (name, event) {
+		const height = 80 + (this[name].length * 30) + 10;
+
+		$('header').css({
+			'height' : height + 'px'
+		});
+
+		this.onSectionSelected('', event);
+		this.timeout(() => {this.resizeInputs();}, 400);
+
+		this.root.$broadcast('expandedHeader', { name : name });
+	}
+
+	contractHeader (name, event) {
+		const height = (this.isMobile) ? 120 : 80;
+
+		$('header').css({
+			'height' : height + 'px'
+		});
+
+
+		this.onSectionSelected('', event);
+		this.timeout(() => {this.resizeInputs();}, 400);
+
+		this.root.$broadcast('contractedHeader', { name : name });
+	}
+
+	resizeInputs () {
+		const marginLeft = 20;
+		const marginTop = (this.isMobile) ? 10 : 15;
+
+		const widthProducto   = $('[nav-option="'+'producto'+'"]').width() + marginLeft;
+		const widthImportador = $('[nav-option="'+'importador'+'"]').width() + marginLeft;
+		const widthExportador = $('[nav-option="'+'exportador'+'"]').width() + marginLeft;
+
+		const heightProducto   = $('[nav-option="'+'producto'+'"]').height() + marginTop;
+		const heightImportador = $('[nav-option="'+'importador'+'"]').height() + marginTop;
+		const heightExportador = $('[nav-option="'+'exportador'+'"]').height() + marginTop;
+
+		const offsetProducto = $('[nav-option="'+'producto'+'"]').offset();
+		const offsetImportador = $('[nav-option="'+'importador'+'"]').offset();
+		const offsetExportador = $('[nav-option="'+'exportador'+'"]').offset();
+
+		$('[input-select-options="'+'producto'+'"]').css({
+			'width': widthProducto, 
+			'left' : offsetProducto.left,
+			'top' : offsetProducto.top + heightProducto
+		});
+
+		$('[input-select-options="'+'importador'+'"]').css({
+			'width': widthImportador, 
+			'left' : offsetImportador.left,
+			'top' : offsetImportador.top + heightImportador
+		});
+
+		$('[input-select-options="'+'exportador'+'"]').css({
+			'width': widthExportador, 
+			'left' : offsetExportador.left,
+			'top' : offsetExportador.top + heightExportador
+		});
+	}
 };
 
 const name = 'navigationInputs';
